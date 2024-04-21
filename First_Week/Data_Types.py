02f1214ea56e162dbd2d1149355cbbcdc3e51117
Inaam Jr <inaamjr@gmail.com>
2024-04-21 13:16:44 +0530

    Week 1 final changes

--- First_Week/Data_Types.py
+++ First_Week/Data_Types.py
@@ -13,6 +13,7 @@ z = """\nHello World... I'm a proud Sri Lankan :)"""
 
 
 
+#intiger
 a = 0
 
 #check the type
@@ -26,7 +27,81 @@ a = 0
 
 
 
+#float
 b = 0.0
-print(type(b))
 
-print("Configure Git")
+#print(type(b))
+
+#Github Test
+#print("Configure Git")
+
+
+
+#list
+l1 = [1, 2, 3, "Jr", 0.0]
+
+#print the 4th element in the array l1
+#print(l1[3])
+
+#print first four elements of the array l1
+#print(l1[0:4])
+
+#to fint the length of the array l1
+#print(len(l1))
+
+
+
+#tuple
+tuple1 = (1, 2, 3, 4)
+
+#print tuple1
+#print(tuple1)
+
+
+
+#boolean
+is_created = False
+is_failed = True
+
+#print variable and its data type
+#print(is_created, type(is_created))
+
+
+
+#set
+set1 = set([1, 2, 'Geeks', 4, 'For', 6, 'Geeks'])
+
+#print(set1)
+
+
+
+#dictionary
+dict = {}
+dict1 = {
+    "name": "Inaam",
+    "age": 16,
+    "mobile": '0764935825'
+}
+
+dict2 = {
+    "name": "Ahamed",
+    "age": 18,
+    "mobile": '0764935825'
+}
+
+
+#print(dict1['mobile'])
+
+list = []
+list.append(dict1)
+list.append(dict2)
+#print(list)
+
+
+
+import pandas as pd
+
+df = pd.DataFrame(list)
+print(df.head())
+print(type(df))
+
