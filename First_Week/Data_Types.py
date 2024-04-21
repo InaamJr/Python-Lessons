d850f627d7c38dd92e8dd57127ec011eb88d322d
Inaam Jr <inaamjr@gmail.com>
2024-04-21 11:42:39 +0530

    trying datatypes

---
+++ First_Week/Data_Types.py
@@ -0,0 +1,30 @@
+x = "Python{}"       #{}are used to pass a value for a string
+y = "Lion"
+z = """\nHello World... I'm a proud Sri Lankan :)"""
+
+#check the type
+#print(type(x), x)
+
+
+#print(x, y, z)
+
+#print a letter of variable x according to indexes
+#print(x[-2])
+
+
+
+a = 0
+
+#check the type
+#print(type(a))
+
+
+#print(x.format(a))   #.format is used to pass the int value of "a" for x
+
+#another method to use the .format method
+#print(f"Inaam Ahamed {y}")
+
+
+
+b = 0.0
+print(type(b))
