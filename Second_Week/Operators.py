e73de1105d76d8462ad3162ce2983799b48a7eaf
Inaam Jr <inaamjr@gmail.com>
2024-05-03 15:36:37 +0530

    Second Week

---
+++ Second_Week/Operators.py
@@ -0,0 +1,124 @@
+# x = 10
+# y = 3
+
+# print ("x%y" , x%y)
+# print ("x+y" , x+y)
+# print ("x-y" , x-y)
+# print ("x*y" , x*y)
+# print ("x**y" , x**y)
+# print ("x/y" , x/y)
+# print ("x//y" , x//y)
+
+
+
+# -----------------Basic Calculator--------------------
+
+# No1 = int(input("Enter a number: "))
+# No2 = int(input("Enter another number: "))
+
+# print(str(No1%No2))
+# print(str(No1+No2))
+# print(str(No1-No2))
+# print(str(No1*No2))
+# print(str(No1**No2))
+# print(str(No1/No2))
+# print(str(No1//No2))
+
+#------------------------------------------------------
+
+
+
+# ---------------Finding the Max number----------------
+
+# x = 5
+# y = 20
+# z = 100
+
+# Initialize a variable to store the maximum value
+# max_value = x
+
+# Check if y is greater than the current max_value
+# if y > max_value:
+#     max_value = y
+
+# Check if z is greater than the current max_value
+# if z > max_value:
+#     max_value = z
+
+# Print the maximum value
+# print("The highest value is:", max_value)
+
+
+
+# ----2nd Method ----
+
+# x = 5
+# y = 20
+# z = 100
+
+# Finding the maximum value among x, y, and z
+# max_value = max(x, y, z)
+
+# Printing the maximum value
+# print("The highest value is:", max_value)
+
+#------------------------------------------------------
+
+
+
+#------------------------------------------------------
+
+# x = [5, 6, 7]
+# y = int(5)
+# z = float(6)
+
+# if y in x and y>z:
+#     print("Hello")
+# else:
+#     print("? ")
+
+#------------------------------------------------------
+
+
+#------------------- For Loop -------------------------
+
+# x = [5, 6, 7]
+
+# for i in x:
+#     print(i)
+    
+# for index, i in enumerate(x):
+#     0, 5
+#     1, 6
+#     2, 7
+    
+#     if 6 == i:
+#         print("index", index)
+    
+#------------------------------------------------------ 
+
+# for i in range(2, 10, 2):
+#     print(i)
+
+#------------------------------------------------------
+
+
+
+#----------------- While Loop -------------------------
+
+# x = "Python"
+# count = 1
+
+# while True:
+#     if count < 1000000:
+#         print(x*count)
+#     else:
+#         break
+#     count = count + 1
+    
+#------------------------------------------------------
+
+
+
+    
+    
